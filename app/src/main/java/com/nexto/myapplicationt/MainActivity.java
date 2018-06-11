7df101f59f61d90798a783e36a6edd80f2da67b6
yuchunxiang <857760313@qq.com>
2018-06-11 18:18:28 +0800

    Merge https://github.com/q8577/Hello

--- app/src/main/java/com/nexto/myapplicationt/MainActivity.java
+++ app/src/main/java/com/nexto/myapplicationt/MainActivity.java
@@ -27,6 +27,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     Handler handler = new Handler() {
@@ -34,17 +36,23 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
 //            textView.setText(json);
-            Log.d("aaaa", json);
+            Log.d("aaaa", "进入handle");
 //绘制脸部识别点
             bitmap=bitmap.copy(Bitmap.Config.ARGB_8888, true);
             Canvas canvas = new Canvas(bitmap);
             Paint paint = new Paint();
             paint.setColor(Color.RED);
-            paint.setStrokeWidth(2);//宽度
-            canvas.drawPoints(points,paint);
+            paint.setStrokeWidth(20);//宽度
+//            canvas.drawPoints(points,paint);
+            for (int i=0;i<points.length/2;i++) {
+                canvas.drawCircle(points[2*i],points[2*i+1],10,paint);
+
+            }
             canvas.save();
             ImageView iv =findViewById(R.id.iv);
             iv.setImageBitmap(bitmap);
+            Log.d("aaaa", "绘图完成");
+
 
         }
     };
@@ -59,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("aaaa", "oncreat()");
+
         initView();
         Log.d("aaaa", "initview完成");
 
@@ -99,7 +109,10 @@ public class MainActivity extends AppCompatActivity {
                 FaceLandMarkDetector faceLMDetector = new FaceLandMarkDetector(MainActivity.this);
                 //进行五官特征检测；
                 resultObj = faceLMDetector.detectLandMark(frame, null);
+                Log.d("aaa", "开始转换");
 //                List<FaceLandmark> landMarks = faceLMDetector.convertResult(resultObj);//得到java类型结果  --出错
+                Log.d("aaa", "转换通过");
+
                 //获取第一个点的坐标
 //                PointF p = landMarks.get(0).getPositionF();
 
@@ -109,18 +122,25 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONArray landmarks = null;
                 try {
-                    resultObj = new JSONObject("{\"landmark\":[{\"positionF\":{\"x\":91.918335,\"y\":311.54224},\"type\":-1},{\"positionF\":{\"x\":95.64966,\"y\":356.20996},\"type\":-1},{\"positionF\":{\"x\":103.22046,\"y\":400.76953},\"type\":-1},{\"positionF\":{\"x\":114.684814,\"y\":443.38232},\"type\":-1},{\"positionF\":{\"x\":130.25903,\"y\":483.3994},\"type\":-1},{\"positionF\":{\"x\":151.24097,\"y\":520.3882},\"type\":-1},{\"positionF\":{\"x\":176.87354,\"y\":553.0508},\"type\":-1},{\"positionF\":{\"x\":208.6709,\"y\":579.2241},\"type\":-1},{\"positionF\":{\"x\":250.52661,\"y\":587.44385},\"type\":-1},{\"positionF\":{\"x\":293.89648,\"y\":578.3589},\"type\":-1},{\"positionF\":{\"x\":329.3711,\"y\":553.0508},\"type\":-1},{\"positionF\":{\"x\":359.438,\"y\":520.8208},\"type\":-1},{\"positionF\":{\"x\":384.09717,\"y\":483.83203},\"type\":-1},{\"positionF\":{\"x\":401.18555,\"y\":442.08447},\"type\":-1},{\"positionF\":{\"x\":411.35205,\"y\":396.1189},\"type\":-1},{\"positionF\":{\"x\":417.625,\"y\":348.7473},\"type\":-1},{\"positionF\":{\"x\":420.65332,\"y\":300.83496},\"type\":-1},{\"positionF\":{\"x\":114.684814,\"y\":283.63843},\"type\":-1},{\"positionF\":{\"x\":131.88135,\"y\":263.1432},\"type\":-1},{\"positionF\":{\"x\":158.70361,\"y\":255.84277},\"type\":-1},{\"positionF\":{\"x\":187.79712,\"y\":257.14062},\"type\":-1},{\"positionF\":{\"x\":215.70093,\"y\":264.71143},\"type\":-1},{\"positionF\":{\"x\":259.71973,\"y\":261.14233},\"type\":-1},{\"positionF\":{\"x\":289.5703,\"y\":250.8136},\"type\":-1},{\"positionF\":{\"x\":321.8003,\"y\":247.2445},\"type\":-1},{\"positionF\":{\"x\":352.94873,\"y\":253.24707},\"type\":-1},{\"positionF\":{\"x\":375.66113,\"y\":272.76892},\"type\":-1},{\"positionF\":{\"x\":238.62964,\"y\":281.69165},\"type\":-1},{\"positionF\":{\"x\":238.19702,\"y\":305.05298},\"type\":-1},{\"positionF\":{\"x\":237.22363,\"y\":328.198},\"type\":-1},{\"positionF\":{\"x\":236.46655,\"y\":352.85718},\"type\":-1},{\"positionF\":{\"x\":211.48291,\"y\":383.89746},\"type\":-1},{\"positionF\":{\"x\":225.65112,\"y\":388.3318},\"type\":-1},{\"positionF\":{\"x\":240.57642,\"y\":390.60303},\"type\":-1},{\"positionF\":{\"x\":255.82617,\"y\":386.92578},\"type\":-1},{\"positionF\":{\"x\":270.75146,\"y\":381.62622},\"type\":-1},{\"positionF\":{\"x\":149.7268,\"y\":296.40063},\"type\":-1},{\"positionF\":{\"x\":165.08472,\"y\":282.82727},\"type\":-1},{\"positionF\":{\"x\":187.79712,\"y\":280.77234},\"type\":-1},{\"positionF\":{\"x\":206.94043,\"y\":293.26416},\"type\":-1},{\"positionF\":{\"x\":188.66235,\"y\":298.99634},\"type\":-1},{\"positionF\":{\"x\":166.16626,\"y\":301.9165},\"type\":-1},{\"positionF\":{\"x\":278.53857,\"y\":288.938},\"type\":-1},{\"positionF\":{\"x\":297.57373,\"y\":274.4994},\"type\":-1},{\"positionF\":{\"x\":321.15137,\"y\":275.14832},\"type\":-1},{\"positionF\":{\"x\":338.67236,\"y\":287.09937},\"type\":-1},{\"positionF\":{\"x\":321.36768,\"y\":294.02124},\"type\":-1},{\"positionF\":{\"x\":298.00635,\"y\":293.04785},\"type\":-1},{\"positionF\":{\"x\":201.3164,\"y\":467.8252},\"type\":-1},{\"positionF\":{\"x\":211.80737,\"y\":439.70508},\"type\":-1},{\"positionF\":{\"x\":228.89575,\"y\":425.64502},\"type\":-1},{\"positionF\":{\"x\":241.76611,\"y\":428.88965},\"type\":-1},{\"positionF\":{\"x\":255.17725,\"y\":424.34717},\"type\":-1},{\"positionF\":{\"x\":276.15918,\"y\":436.89307},\"type\":-1},{\"positionF\":{\"x\":293.03125,\"y\":462.8501},\"type\":-1},{\"positionF\":{\"x\":277.88965,\"y\":481.45264},\"type\":-1},{\"positionF\":{\"x\":259.2871,\"y\":490.9702},\"type\":-1},{\"positionF\":{\"x\":245.01074,\"y\":492.917},\"type\":-1},{\"positionF\":{\"x\":230.84253,\"y\":492.48438},\"type\":-1},{\"positionF\":{\"x\":213.97046,\"y\":484.48096},\"type\":-1},{\"positionF\":{\"x\":210.40137,\"y\":464.79688},\"type\":-1},{\"positionF\":{\"x\":229.86914,\"y\":444.03125},\"type\":-1},{\"positionF\":{\"x\":242.7395,\"y\":443.59863},\"type\":-1},{\"positionF\":{\"x\":255.82617,\"y\":442.5171},\"type\":-1},{\"positionF\":{\"x\":283.51367,\"y\":460.4707},\"type\":-1},{\"positionF\":{\"x\":257.34033,\"y\":465.87842},\"type\":-1},{\"positionF\":{\"x\":243.82104,\"y\":468.0415},\"type\":-1},{\"positionF\":{\"x\":230.62622,\"y\":466.95996},\"type\":-1}]}");
+                    int resultCode = resultObj.getInt("resultCode");
+                    Log.d("aaaa", "resultCode是："+resultCode);
 
+//                    resultObj = new JSONObject("{\"landmark\":[{\"positionF\":{\"x\":91.918335,\"y\":311.54224},\"type\":-1},{\"positionF\":{\"x\":95.64966,\"y\":356.20996},\"type\":-1},{\"positionF\":{\"x\":103.22046,\"y\":400.76953},\"type\":-1},{\"positionF\":{\"x\":114.684814,\"y\":443.38232},\"type\":-1},{\"positionF\":{\"x\":130.25903,\"y\":483.3994},\"type\":-1},{\"positionF\":{\"x\":151.24097,\"y\":520.3882},\"type\":-1},{\"positionF\":{\"x\":176.87354,\"y\":553.0508},\"type\":-1},{\"positionF\":{\"x\":208.6709,\"y\":579.2241},\"type\":-1},{\"positionF\":{\"x\":250.52661,\"y\":587.44385},\"type\":-1},{\"positionF\":{\"x\":293.89648,\"y\":578.3589},\"type\":-1},{\"positionF\":{\"x\":329.3711,\"y\":553.0508},\"type\":-1},{\"positionF\":{\"x\":359.438,\"y\":520.8208},\"type\":-1},{\"positionF\":{\"x\":384.09717,\"y\":483.83203},\"type\":-1},{\"positionF\":{\"x\":401.18555,\"y\":442.08447},\"type\":-1},{\"positionF\":{\"x\":411.35205,\"y\":396.1189},\"type\":-1},{\"positionF\":{\"x\":417.625,\"y\":348.7473},\"type\":-1},{\"positionF\":{\"x\":420.65332,\"y\":300.83496},\"type\":-1},{\"positionF\":{\"x\":114.684814,\"y\":283.63843},\"type\":-1},{\"positionF\":{\"x\":131.88135,\"y\":263.1432},\"type\":-1},{\"positionF\":{\"x\":158.70361,\"y\":255.84277},\"type\":-1},{\"positionF\":{\"x\":187.79712,\"y\":257.14062},\"type\":-1},{\"positionF\":{\"x\":215.70093,\"y\":264.71143},\"type\":-1},{\"positionF\":{\"x\":259.71973,\"y\":261.14233},\"type\":-1},{\"positionF\":{\"x\":289.5703,\"y\":250.8136},\"type\":-1},{\"positionF\":{\"x\":321.8003,\"y\":247.2445},\"type\":-1},{\"positionF\":{\"x\":352.94873,\"y\":253.24707},\"type\":-1},{\"positionF\":{\"x\":375.66113,\"y\":272.76892},\"type\":-1},{\"positionF\":{\"x\":238.62964,\"y\":281.69165},\"type\":-1},{\"positionF\":{\"x\":238.19702,\"y\":305.05298},\"type\":-1},{\"positionF\":{\"x\":237.22363,\"y\":328.198},\"type\":-1},{\"positionF\":{\"x\":236.46655,\"y\":352.85718},\"type\":-1},{\"positionF\":{\"x\":211.48291,\"y\":383.89746},\"type\":-1},{\"positionF\":{\"x\":225.65112,\"y\":388.3318},\"type\":-1},{\"positionF\":{\"x\":240.57642,\"y\":390.60303},\"type\":-1},{\"positionF\":{\"x\":255.82617,\"y\":386.92578},\"type\":-1},{\"positionF\":{\"x\":270.75146,\"y\":381.62622},\"type\":-1},{\"positionF\":{\"x\":149.7268,\"y\":296.40063},\"type\":-1},{\"positionF\":{\"x\":165.08472,\"y\":282.82727},\"type\":-1},{\"positionF\":{\"x\":187.79712,\"y\":280.77234},\"type\":-1},{\"positionF\":{\"x\":206.94043,\"y\":293.26416},\"type\":-1},{\"positionF\":{\"x\":188.66235,\"y\":298.99634},\"type\":-1},{\"positionF\":{\"x\":166.16626,\"y\":301.9165},\"type\":-1},{\"positionF\":{\"x\":278.53857,\"y\":288.938},\"type\":-1},{\"positionF\":{\"x\":297.57373,\"y\":274.4994},\"type\":-1},{\"positionF\":{\"x\":321.15137,\"y\":275.14832},\"type\":-1},{\"positionF\":{\"x\":338.67236,\"y\":287.09937},\"type\":-1},{\"positionF\":{\"x\":321.36768,\"y\":294.02124},\"type\":-1},{\"positionF\":{\"x\":298.00635,\"y\":293.04785},\"type\":-1},{\"positionF\":{\"x\":201.3164,\"y\":467.8252},\"type\":-1},{\"positionF\":{\"x\":211.80737,\"y\":439.70508},\"type\":-1},{\"positionF\":{\"x\":228.89575,\"y\":425.64502},\"type\":-1},{\"positionF\":{\"x\":241.76611,\"y\":428.88965},\"type\":-1},{\"positionF\":{\"x\":255.17725,\"y\":424.34717},\"type\":-1},{\"positionF\":{\"x\":276.15918,\"y\":436.89307},\"type\":-1},{\"positionF\":{\"x\":293.03125,\"y\":462.8501},\"type\":-1},{\"positionF\":{\"x\":277.88965,\"y\":481.45264},\"type\":-1},{\"positionF\":{\"x\":259.2871,\"y\":490.9702},\"type\":-1},{\"positionF\":{\"x\":245.01074,\"y\":492.917},\"type\":-1},{\"positionF\":{\"x\":230.84253,\"y\":492.48438},\"type\":-1},{\"positionF\":{\"x\":213.97046,\"y\":484.48096},\"type\":-1},{\"positionF\":{\"x\":210.40137,\"y\":464.79688},\"type\":-1},{\"positionF\":{\"x\":229.86914,\"y\":444.03125},\"type\":-1},{\"positionF\":{\"x\":242.7395,\"y\":443.59863},\"type\":-1},{\"positionF\":{\"x\":255.82617,\"y\":442.5171},\"type\":-1},{\"positionF\":{\"x\":283.51367,\"y\":460.4707},\"type\":-1},{\"positionF\":{\"x\":257.34033,\"y\":465.87842},\"type\":-1},{\"positionF\":{\"x\":243.82104,\"y\":468.0415},\"type\":-1},{\"positionF\":{\"x\":230.62622,\"y\":466.95996},\"type\":-1}]}");
+                    resultObj=new JSONObject("{'landmark':"+resultObj.getString("landmark")+"}");
                     landmarks = resultObj.getJSONArray("landmark");
-                    landmarks.getJSONObject(0).getJSONObject("positionF").get("x");
+                    Log.d("aaaa", "获得json数组："+landmarks);
+
+                    double x = (double) landmarks.getJSONObject(0).getJSONObject("positionF").get("x");
+                    Log.d("aaaa", "第一个x是："+x);
+
                     points = new float[68*2];
                     for (int i=0;i<68;i++) {
-                        Log.d("aaaa", "bbbbbbb");
+//                        Log.d("aaaa", "bbbbbbb");
 
-                        points[i*2] = (float) landmarks.getJSONObject(i).getJSONObject("positionF").get("x");
-                        Log.d("aaaa", "ccccccc");
+                        points[i*2] = Float.valueOf(landmarks.getJSONObject(i).getJSONObject("positionF").get("x").toString()) ;
+//                        Log.d("aaaa", "ccccccc");
 
-                        points[i*2+1] = (float) landmarks.getJSONObject(i).getJSONObject("positionF").get("y");
+                        points[i*2+1] =Float.valueOf(landmarks.getJSONObject(i).getJSONObject("positionF").get("y").toString()) ;
                     }
                     Log.d("aaaa", "ddddddddd");
 
@@ -129,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
                 Message obtain = Message.obtain();
-//                handler.sendMessage(obtain);
+                handler.sendMessage(obtain);
 
 
             }
--- entitydetect/src/main/java/com/nexto/entitydetect/MainActivity.java
+++ entitydetect/src/main/java/com/nexto/entitydetect/MainActivity.java
@@ -2,12 +2,116 @@ package com.nexto.entitydetect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.huawei.hiai.nlu.model.ResponseResult;//接口返回的结果类
+import com.huawei.hiai.nlu.sdk.NLUAPIService;//接口服务类
+import com.huawei.hiai.nlu.sdk.NLUConstants;//接口常量类
+import com.huawei.hiai.nlu.sdk.OnResultListener;//异步函数，执行成功的回调结果类
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static HashMap entityNameMap = new HashMap<>();
+
+    static
+    {
+        entityNameMap.put("time", "时间");
+        entityNameMap.put("location", "地点");
+        entityNameMap.put("name", "人名");
+        entityNameMap.put("phoneNum", "电话号码");
+        entityNameMap.put("email", "邮箱");
+        entityNameMap.put("url", "url");
+        entityNameMap.put("movie", "电影");
+        entityNameMap.put("tv", "电视剧");
+        entityNameMap.put("varietyshow", "综艺");
+        entityNameMap.put("anime", "动漫");
+        entityNameMap.put("league", "联赛");
+        entityNameMap.put("team", "球队");
+        entityNameMap.put("music", "单曲");
+        entityNameMap.put("musicAlbum", "专辑");
+        entityNameMap.put("singer", "歌手");
+        entityNameMap.put("trainNo", "火车车次");
+        entityNameMap.put("flightNo", "航班号");
+        entityNameMap.put("expressNo", "快递单号");
+        entityNameMap.put("idNo", "证件号");
+        entityNameMap.put("verificationCode", "验证码");
+        entityNameMap.put("app", "手机应用");
+        entityNameMap.put("carNo", "车牌号");
+        entityNameMap.put("bankCardNo", "银行卡号");
+        entityNameMap.put("book", "图书");
+        entityNameMap.put("cate", "菜名");
+        entityNameMap.put("famousBrand", "名牌");
+        entityNameMap.put("stockName", "股票名");
+        entityNameMap.put("stockCode", "股票代码");
+        entityNameMap.put("fundName", "基金名");
+        entityNameMap.put("fundCode", "基金代码");
+    }
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
+
+        NLUAPIService.getInstance().init(MainActivity.this , new OnResultListener<Integer>(){
+
+            @Override
+            public void onResult(Integer result)
+            {
+                // 初始化成功回调，在服务出初始化成功调用该函数
+            }
+        },true);
+
+    }
+
+    public void pase(View view) {
+        String requestJson = "{text:'我要看电影魔兽',module:'movie'}";
+//        ResponseResult respResult = NLUAPIService.getInstance().getEntity(requestJson, NLUConstants.REQUEST_TYPE_LOCAL);
+//        if (null != respResult)
+//        {
+            //获取接口返回结果，参考接口文档返回使用
+//            String result = respResult.getJsonRes();
+            String result = "{\"code\":0,\"message\":\"成功\",\"entity\":{\"time\":[{\"timestamp\":1456559721232,\"oriText\":\"明天\",\"section\":\"D\",\"charOffset\":0},{\"timestamp\":1456559721232,\"oriText\":\"晚上\",\"section\":\"E\",\"charOffset\":10}],\"movie\":[{\"name\":\"机器之血\",\"charOffset\":5}]}}";
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONObject entity=jsonObject.getJSONObject("entity");
+                Iterator<String> entityNames = entity.keys();
+                StringBuffer buffer = new StringBuffer();
+                while (entityNames.hasNext()) {
+                    String name = entityNames.next();
+                    buffer.append(entityNameMap.get(name)+":");
+                    JSONArray jsonArray = entity.getJSONArray(name);
+                    for (int i=0;i<jsonArray.length();i++) {
+                       buffer.append(jsonArray.getJSONObject(i).getString("oriText"));
+                       if (i == jsonArray.length() - 1) {
+                           buffer.append(".\n");
+                       } else {
+                           buffer.append(",");
+                       }
+                    }
+                }
+                result = buffer.toString();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            textView.setText(result);
+            Log.d("aaa", result);
+
+//        }
+//        Module ----可选参数 ，不填的时候所有实体都分析
+
+
     }
 }
